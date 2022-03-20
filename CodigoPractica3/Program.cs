a283749f5263288dc631fc8b63b229007528c76a
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-03-20 16:32:30 -0600

    Add files via upload

---
+++ CodigoPractica3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+namespace Practica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Mistitulos = new List<string>();
+            HtmlWeb oweb = new HtmlWeb();
+            HtmlDocument doc = oweb.Load("https://hdeleon.net/");
+            //HtmlNode Body = doc.DocumentNode.CssSelect("head").First();
+            //string sBody = Body.InnerHtml;
+            foreach (var Nodo in doc.DocumentNode.CssSelect(".entry-title"))
+            {
+                var NodoAnchor = Nodo.CssSelect("a").First();
+                Mistitulos.Add(NodoAnchor.InnerHtml);
+            }
+
+        }
+    }
+}
+
