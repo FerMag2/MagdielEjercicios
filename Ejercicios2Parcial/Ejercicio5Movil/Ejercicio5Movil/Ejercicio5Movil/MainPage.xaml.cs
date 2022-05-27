d71760284304de8b480dfc25eca4602bb3edcc3c
FerMag2 <73145422+FerMag2@users.noreply.github.com>
2022-05-27 10:11:33 -0500

    Ejercicio5Movil
    Ejercicio5Movil

---
+++ Ejercicios2Parcial/Ejercicio5Movil/Ejercicio5Movil/Ejercicio5Movil/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio5Movil
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int contador = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            contador ++;
+            ((Button)sender).Text = "Presioado "+  contador+" veces";
+            
+            
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
